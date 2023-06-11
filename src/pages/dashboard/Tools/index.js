9cef276878c8aa2c9176a53ac81666be64122e3b
Stephen Dang <nhan.dang.work@gmail.com>
2023-06-11 20:56:52 +0700

    Added Task Reporting

--- src/pages/dashboard/Tools/index.js
+++ src/pages/dashboard/Tools/index.js
@@ -28,40 +28,89 @@ const GIT_ACTIONS = [
   { label: "push origin", value: 4 },
 ];
 
+const TYPE = [
+  { label: "feature", value: 0 },
+  { label: "bugfix", value: 1 },
+];
+
 export default function Wallet() {
   const { themeStretch } = useSettings();
 
-  const [taskNumber, setTaskNumber] = useState("");
-  const [inputValue, setInputValue] = useState("");
+  const [data, setData] = useState({
+    usNumber: "",
+    taskNumberBF: "",
+    taskNumberTR: "",
+    taskNameBF: "",
+    taskNameTR: "",
+    gitAction: 0,
+    type: 0,
+  });
+  const [taskReportList, setTaskReportList] = useState("");
   const [label, setLabel] = useState("");
-  const [selectedGitAction, setSelectedGitAction] = useState("");
   const [strGitAction, setStrGitAction] = useState("");
 
   useEffect(() => {
-    const string = GIT_ACTIONS.find((item) => item.value === selectedGitAction);
+    const string = GIT_ACTIONS.find((item) => item.value === data.gitAction);
     if (string?.value === 0) {
       setStrGitAction("");
     } else {
       setStrGitAction(string?.label);
     }
-  }, [selectedGitAction]);
+  }, [data]);
 
   useEffect(() => {
-    const combineString = taskNumber + " " + inputValue;
+    const combineString = data.taskNumberBF + " " + data.taskNameBF;
     const stringnew = combineString.trim().replace(/  +/g, " ").toLowerCase();
     setLabel(stringnew.replaceAll(" ", "-"));
-  }, [taskNumber, inputValue]);
+  }, [data]);
 
-  const updateTaskNumberValue = (e) => {
-    setTaskNumber(e.target.value);
+  const handleOnChange = (e) => {
+    const value = e.target.value;
+    const id = e.target.name;
+
+    setData({
+      ...data,
+      [id]: value,
+    });
   };
 
-  const updateInputValue = (e) => {
-    setInputValue(e.target.value);
+  const handleAddTaskReportList = () => {
+    const obj = {
+      usNumber: data.usNumber,
+      taskNumberTR: data.taskNumberTR,
+      taskNameTR: data.taskNameTR,
+      type: data.type,
+    };
+
+    setTaskReportList((prevArray) => [...prevArray, obj]);
+    setData({
+      ...data,
+      usNumber: "",
+      taskNumberTR: "",
+      taskNameTR: "",
+      type: 0,
+    });
   };
 
-  const handleOnChangeSelect = (e) => {
-    setSelectedGitAction(e.target.value);
+  const handleCopyTaskReportList = () => {
+    const reportList = taskReportList
+      .map((item) => {
+        let string;
+        if (item.type === 0) {
+          string = `User Story ${item.usNumber} - Task ${item.taskNumberTR}: ${item.taskNameTR}`;
+        } else {
+          string = `Bugfix ${item.taskNumberTR}: ${item.taskNameTR}`;
+        }
+
+        return string;
+      })
+      .join("\n");
+
+    return reportList;
+  };
+
+  const handleClear = () => {
+    setTaskReportList([]);
   };
 
   const featureText = `${
@@ -74,7 +123,7 @@ export default function Wallet() {
   return (
     <Page title="General: Tools">
       <Container maxWidth={themeStretch ? false : "xl"}>
-        <Card sx={{ p: 3 }}>
+        <Card sx={{ p: 3, mb: 4 }}>
           <Stack spacing={3}>
             <Grid container spacing={3}>
               <Grid item xs={12} md={12}>
@@ -82,7 +131,12 @@ export default function Wallet() {
               </Grid>
 
               <Grid item xs={12} md={2}>
-                <TextField fullWidth onChange={updateTaskNumberValue} />
+                <TextField
+                  label="Task Number"
+                  name="taskNumberBF"
+                  fullWidth
+                  onChange={handleOnChange}
+                />
               </Grid>
               <Grid item xs={12} md={2}>
                 <FormControl fullWidth>
@@ -92,10 +146,11 @@ export default function Wallet() {
                   <Select
                     labelId="demo-simple-select-label"
                     id="demo-simple-select"
-                    value={selectedGitAction}
+                    name="gitAction"
+                    value={data.gitAction}
                     label="Git Actions"
                     fullWidth
-                    onChange={handleOnChangeSelect}
+                    onChange={handleOnChange}
                   >
                     {GIT_ACTIONS.map((item, index) => {
                       return (
@@ -108,11 +163,17 @@ export default function Wallet() {
                 </FormControl>
               </Grid>
               <Grid item xs={12} md={8}>
-                <TextField fullWidth onChange={updateInputValue} />
+                <TextField
+                  label="Task Name"
+                  name="taskNameBF"
+                  fullWidth
+                  onChange={handleOnChange}
+                />
               </Grid>
 
               <Grid item xs={24} md={8}>
                 <Button
+                  variant="outlined"
                   onClick={() => navigator.clipboard.writeText(featureText)}
                   sx={{ mr: 2 }}
                 >
@@ -123,6 +184,7 @@ export default function Wallet() {
 
               <Grid item xs={24} md={8}>
                 <Button
+                  variant="outlined"
                   onClick={() => navigator.clipboard.writeText(bugfixText)}
                   sx={{ mr: 2 }}
                 >
@@ -130,6 +192,115 @@ export default function Wallet() {
                 </Button>
                 <label>{bugfixText}</label>
               </Grid>
+            </Grid>
+          </Stack>
+        </Card>
+
+        <Card sx={{ p: 3, mb: 4 }}>
+          <Stack spacing={3}>
+            <Grid container spacing={3}>
+              <Grid item xs={12} md={12}>
+                <h2>Task Reporting</h2>
+              </Grid>
+
+              <Grid item xs={12} md={2}>
+                <TextField
+                  label="User Story Number"
+                  name="usNumber"
+                  value={data.usNumber}
+                  fullWidth
+                  onChange={handleOnChange}
+                />
+              </Grid>
+              <Grid item xs={12} md={2}>
+                <TextField
+                  label="Task Number"
+                  name="taskNumberTR"
+                  value={data.taskNumberTR}
+                  fullWidth
+                  onChange={handleOnChange}
+                />
+              </Grid>
+              <Grid item xs={12} md={2}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">Type</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    name="type"
+                    value={data.type}
+                    label="Type"
+                    fullWidth
+                    onChange={handleOnChange}
+                  >
+                    {TYPE.map((item, index) => {
+                      return (
+                        <MenuItem key={index} value={item.value}>
+                          {item.label}
+                        </MenuItem>
+                      );
+                    })}
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  label="Task Name"
+                  name="taskNameTR"
+                  value={data.taskNameTR}
+                  fullWidth
+                  onChange={handleOnChange}
+                />
+              </Grid>
+
+              <Grid item xs={12} md={9}></Grid>
+
+              <Grid item xs={12} md={3} style={{ display: "flex" }}>
+                <Button
+                  variant="outlined"
+                  onClick={() =>
+                    navigator.clipboard.writeText(handleCopyTaskReportList())
+                  }
+                  disabled={taskReportList.length <= 0}
+                  fullWidth
+                  sx={{ mr: 2 }}
+                >
+                  Copy
+                </Button>
+                <Button
+                  variant="outlined"
+                  onClick={handleClear}
+                  disabled={taskReportList.length <= 0}
+                  fullWidth
+                  sx={{ mr: 2 }}
+                >
+                  Clear
+                </Button>
+                <Button
+                  onClick={handleAddTaskReportList}
+                  fullWidth
+                  variant="outlined"
+                >
+                  Add
+                </Button>
+              </Grid>
+
+              {taskReportList.length > 0 &&
+                taskReportList.map((item) => {
+                  if (item.type === 0) {
+                    return (
+                      <Grid item xs={24} md={12}>
+                        <label>{`User Story ${item.usNumber} - Task ${item.taskNumberTR}: ${item.taskNameTR}`}</label>
+                      </Grid>
+                    );
+                  } else {
+                    return (
+                      <Grid item xs={24} md={12}>
+                        <label>{`Bugfix ${item.taskNumberTR}: ${item.taskNameTR}`}</label>
+                      </Grid>
+                    );
+                  }
+                })}
             </Grid>
           </Stack>
         </Card>
